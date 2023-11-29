d7b014fb3ef9f4973db5f8bd2ccb4b83e6304dc4
Viinu <viiniciuaramos88@gmail.com>
2023-11-28 22:14:55 -0300

    feat: adicionando códigos do quickstart

---
+++ projetoapi/src/test/java/com/github/libraly/projetoapi/ProjetoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.libraly.projetoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
